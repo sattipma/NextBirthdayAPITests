47410b9102c2844e7de3fac5ef869b2fd3dbd654
satish.telukuntla <satish.telukuntla@justanswer.com>
2023-04-15 02:59:43 +0530

    removed unwanted code

--- src/test/java/TestUtilities.java
+++ src/test/java/TestUtilities.java
@@ -43,46 +43,7 @@ public class TestUtilities {
         }
     }
 
-    public static String timeLeftUntilNextBirthday1(String dob, String unit) {
-        Calendar today = Calendar.getInstance();
-        Calendar birthDate = new GregorianCalendar();
-        String[] dobParts = dob.split("-");
-        int year = Integer.parseInt(dobParts[0]);
-        int month = Integer.parseInt(dobParts[1]) - 1; // Month index starts from 0
-        int day = Integer.parseInt(dobParts[2]);
-        birthDate.set(year, month, day, 0, 0, 0);
-
-        if (birthDate.after(today)) {
-            birthDate.set(Calendar.YEAR, today.get(Calendar.YEAR));
-        } else if (birthDate.get(Calendar.MONTH) == today.get(Calendar.MONTH) && birthDate.get(Calendar.DAY_OF_MONTH) <= today.get(Calendar.DAY_OF_MONTH)) {
-            birthDate.set(Calendar.YEAR, today.get(Calendar.YEAR) + 1);
-        } else {
-            birthDate.set(Calendar.YEAR, today.get(Calendar.YEAR));
-        }
-
-        long diffMillis = birthDate.getTimeInMillis() - today.getTimeInMillis();
-        long timeLeft = 0;
-
-        switch (unit) {
-            case "hour":
-                timeLeft = TimeUnit.MILLISECONDS.toHours(diffMillis);
-                return timeLeft + " hours left";
-            case "day":
-                timeLeft = TimeUnit.MILLISECONDS.toDays(diffMillis) + 1;
-                return timeLeft + " days left";
-            case "week":
-                timeLeft = (TimeUnit.MILLISECONDS.toDays(diffMillis) + 1) / 7;
-                return timeLeft + " weeks left";
-            case "month":
-                int yearDiff = birthDate.get(Calendar.YEAR) - today.get(Calendar.YEAR);
-                int monthDiff = birthDate.get(Calendar.MONTH) - today.get(Calendar.MONTH);
-                timeLeft = yearDiff * 12 + monthDiff;
-                return timeLeft + " months left";
-            default:
-                return "Invalid time unit: " + unit;
-        }
-    }
-
+    
     @DataProvider(name = "BirthdayPositiveTestData")
     public Object[][] getData(){
         return new Object[][] {
